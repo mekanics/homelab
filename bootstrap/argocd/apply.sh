4efe1f26053d31d4afb71364f60fd05f0f796392
Alexandre Joly <alexandre.joly@kilokilo.ch>
2022-09-14 21:59:22 +0200

    Lint

--- bootstrap/argocd/apply.sh
+++ bootstrap/argocd/apply.sh
@@ -3,9 +3,9 @@
 helm template \
     --include-crds \
     --namespace argocd \
-    argocd . \
-    | kubectl apply -n argocd -f -
+    argocd . |
+    kubectl apply -n argocd -f -
 
 kubectl -n argocd wait --timeout=60s --for condition=Established \
-       crd/applications.argoproj.io \
-       crd/applicationsets.argoproj.io
+    crd/applications.argoproj.io \
+    crd/applicationsets.argoproj.io
