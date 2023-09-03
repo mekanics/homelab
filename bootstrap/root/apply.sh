1b4d8b723384de6fe2ebe410805df1e233441ec7
Alexandre Joly <alexandre.joly@j2y.dev>
2023-09-03 17:41:36 +0200

    Add argocd seed values and apply argocd configuration

--- bootstrap/root/apply.sh
+++ bootstrap/root/apply.sh
@@ -2,12 +2,12 @@
 
 VALUES="values.yaml"
 
-kubectl get ingress gitea --namespace gitea ||
-    VALUES="values-seed.yaml"
+kubectl get ingress gitea --namespace gitea \
+    || VALUES="values-seed.yaml"
 
 helm template \
     --include-crds \
     --namespace argocd \
     --values "${VALUES}" \
-    argocd . |
-    kubectl apply -n argocd -f -
+    argocd . \
+    | kubectl apply -n argocd -f -
